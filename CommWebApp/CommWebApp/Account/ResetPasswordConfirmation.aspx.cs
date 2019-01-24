e3b49349bef6e1c243a81dfc8dacc22c78c0c8ce
Filip Lyesovyy <lyesovyyf@gmail.com>
2019-01-24 16:33:24 -0500

    forms

---
+++ CommWebApp/CommWebApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CommWebApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CommWebApp/CommWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommWebApp.Startup))]
+namespace CommWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
